9f00f6523a168301163f5b7a754fcc1f15da9bdb
Pallavi Gaikwad <pallavigaikwad24.pg@gmail.com>
2023-11-11 09:05:34 +0530

    commited

---
+++ Strings/Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+    public static void main(String[] args) {
+
+        String str = "car";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                System.out.println("This is not palindrome");
+                return;
+            }
+        }
+
+        System.out.println("This is palindrome");
+        return;
+    }
+
+}
